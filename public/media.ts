f0aa4da59920d158310884e6ff979ead3c439284
anvin-shaiju10 <anvinshaiju@gmail.com>
2025-12-05 07:38:13 +0530

    update

---
+++ public/media.ts
@@ -0,0 +1,36 @@
+
+export const MEDIA = {
+  video: {
+    hero: "https://videos.pexels.com/video-files/8000624/8000624-hd_1920_1080_25fps.mp4",
+    craftsmanship: "https://videos.pexels.com/video-files/4624797/4624797-hd_1920_1080_30fps.mp4"
+  },
+  images: {
+    hero: {
+      main: "/dibakar-roy-LM8K3sTei1U-unsplash.jpg"
+    },
+    product: {
+      sareeKasavu: "/one.jpg",
+      sareeWhite: "/two.jpg",
+      sareeModel: "/three.jpg",
+      linenRaw: "/four.jpg",
+      kurtaBeige: "/five.jpg",
+      jewellery: "/one.jpg",
+      details: "/two.jpg",
+      emeraldFabric: "/three.jpg",
+      bridalRed: "/four.jpg",
+      bridalGold: "/five.jpg"
+    },
+    sections: {
+      craftsmanshipPoster: "/one.jpg",
+      aboutHero: "/two.jpg",
+      newsletter: "/three.jpg",
+      curatedArrivals: [
+        "/six.jpg",
+        "/seven.jpg",
+        "/eight.jpg",
+        "/nine.jpg",
+        "/ten.jpg"
+      ]
+    }
+  }
+};
